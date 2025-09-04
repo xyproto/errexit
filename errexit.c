5ddf61fd84317e4733e5cfaeaf503478660e9340
Alexander F. Rødseth <rodseth@gmail.com>
2025-09-04 10:04:03 +0200

    Always exit with error code 141 + add a version flag

--- errexit.c
+++ errexit.c
@@ -7,26 +7,28 @@
 #include <sys/types.h>
 #include <getopt.h>
 
-#define VERSION "1.0.0"
+#define VERSION "1.0.1"
 
 static void usage(void) {
     fprintf(stderr, "usage: errexit [OPTIONS] string [exit_code]\n");
     fprintf(stderr, "  -e, --error     prefix with 'error: '\n");
     fprintf(stderr, "  -E, --Error     prefix with 'Error: '\n");
+    fprintf(stderr, "  -v, --version   show version\n");
     fprintf(stderr, "  -h, --help      show this help\n");
     exit(1);
 }
 
 int main(int argc, char *argv[]) {
     const char *prefix = "";
-    
+
     static struct option long_options[] = {
-        {"error", no_argument, 0, 'e'},
-        {"Error", no_argument, 0, 'E'},
-        {"help",  no_argument, 0, 'h'},
+        {"error",   no_argument, 0, 'e'},
+        {"Error",   no_argument, 0, 'E'},
+        {"version", no_argument, 0, 'v'},
+        {"help",    no_argument, 0, 'h'},
         {0, 0, 0, 0}
     };
-    
+
     int opt;
     while ((opt = getopt_long(argc, argv, "eEvh", long_options, NULL)) != -1) {
         switch (opt) {
@@ -47,31 +49,21 @@ int main(int argc, char *argv[]) {
             usage();
         }
     }
-    
+
     if (optind >= argc) {
         usage();
     }
-    
+
     const char *message = argv[optind];
-    int exit_code = 1;
-    
-    if (optind + 1 < argc) {
-        char *endptr;
-        exit_code = (int)strtol(argv[optind + 1], &endptr, 10);
-        if (*endptr != '\0') {
-            fprintf(stderr, "errexit: invalid exit code\n");
-            exit(1);
-        }
-    }
-    
+
     fprintf(stderr, "%s%s\n", prefix, message);
-    
+
     pid_t ppid = getppid();
     if (ppid > 1) {
-        if (kill(ppid, SIGTERM) != 0) {
+        if (kill(ppid, SIGPIPE) != 0) {
             kill(ppid, SIGKILL);
         }
     }
-    
-    exit(exit_code);
+
+    exit(1);
 }
